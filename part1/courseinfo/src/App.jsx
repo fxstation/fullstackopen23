94f2188a7170a8de32003235b74bc9d9d6b8e95b
marco <marco@fxstation.org>
2023-11-14 22:54:56 +0800

    Exercise 1.1 Refactoring Components 2/3 Content

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,15 +10,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
       {/* <Content part={part1} exercise={exercises1}/>
       <Content part={part2} exercise={exercises2}/>
@@ -34,4 +28,11 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  return(
+  <p>
+    {props.part} {props.exercises}
+  </p>
+  )
+}
 export default App
