ff8e789299f31ecc0cdb69843c4420b8667a9a5e
marco <marco@fxstation.org>
2023-11-24 10:43:05 +0800

    Completed exercise1.3 to refactor part variables

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,19 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name : 'Fundamentals of React',
+    exercises : 10
+  }
+  const part2 = {
+    name : 'Using props to pass data',
+    exercises : 7
+  }
+  const part3 = {
+    name : 'State of a component',
+    exercises : 14
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
@@ -25,16 +31,18 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return(
   <div>
-    <Part part={props.part}/>
+    <Part part={props.part.name}/>
     <Part part=" "/>
-    <Part part={props.exercises}/>
+    <Part part={props.part.exercises}/>
   </div>
   )
 }
 
 const Part = (props) => {
+  console.log(props)
   return(
     props.part
   )
